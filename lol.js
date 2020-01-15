222e95b9282f02b7f4376b8a5237773594a506c7
FallingForYouForever <59911188+FallingForYouForever@users.noreply.github.com>
2020-01-15 16:14:54 +0800

    Just some simple ones,it's ok to have look~
    Just some simple ones,it's ok to have look~

---
+++ lol.js
@@ -0,0 +1,21 @@
+window.onload=function(){
+    
+    var tab= document.getElementById("tab");
+    var topic=tab.getElementsByTagName("li");
+    var content=document.getElementById("content");
+    var div=content.getElementsByTagName("div");
+    var len=topic.length;
+    for(var i=0;i<len;i++)
+    {
+        topic[i].index=i;
+        topic[i].onmouseover=function(){
+            for(i=0;i<len;i++)
+            {
+                topic[i].className='';
+                div[i].className='hide';
+            }
+            topic[this.index].className='act';
+            div[this.index].className='';
+        }
+    }
+}
